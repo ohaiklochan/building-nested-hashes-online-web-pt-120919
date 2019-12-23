edd37ac598660acecfb20365e38642ad49a9381e
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 03:09:05 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,27 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch => {  
+  },
+    :matriarch => {
+  },
+    :hero => {
+      :name => "Romeo",
+      :age => "15"
+  },
+    :hero_friends => [
+      {
+         :name => "Benvolio",
+         :age => "17",
+         :attitude => "worried"
+      },
+      {
+        :name => "Mercutio",
+        :age => "18",
+        :attitude => "hot-headed"
+      }
+    ]
+  },
       }, 
     :capulet => {
       #your key/value pairs here!
